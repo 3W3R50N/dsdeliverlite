3464b83fc784738110960b8a4fb77731634508e3
Ewerson Felix <ewersonfelixx@hotmail.com>
2021-01-05 00:39:23 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsdeliverlite/DsdeliverliteApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdeliverlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdeliverliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdeliverliteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsdeliverlite/DsdeliverliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdeliverlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
