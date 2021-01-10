0d0cb18be8af5309a649abeb0c3daaa751f64362
Ewerson Felix <ewersonfelixx@hotmail.com>
2021-01-10 01:06:32 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
-
 import Routes from './routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>       
-        <Routes />        
+        <Routes />  
+        <ToastContainer />      
     </>
   );
 }
